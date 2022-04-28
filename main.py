d5e725910ffb9135e9e424d357ac7c8ccd0e2c89
Janvier Uwiragiye <uwiragiyejanvi@gmail.com>
2022-04-27 21:27:59 -0400

    statsmodels

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import numpy as np
 
 
 def main():
-    arr1 = np.transpose([1, 5, -3, 2])
-    arr2 = np.transpose([8, 2, 4, 7])
-    result = np.dot(arr1, arr2)
+    A = np.array([[1, 2], [3, 4]])
+
+    print(A[:, -1])
 
 
 if __name__ == '__main__':
